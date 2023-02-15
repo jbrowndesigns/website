a077b61400e37b9632d13e9646049bf09e52e843
jbrowndesigns <brown.jdesign@gmail.com>
2023-02-15 11:56:02 -0800

    +Contact Form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+const openModal = document.querySelector('.open-btn');
+const closeModal = document.querySelector('.close-btn');
+const modal = document.querySelector('.modal');
+const submit = document.querySelector('.submit');
+
+
+openModal.addEventListener('click', ()=>{
+  modal.showModal();  
+  submit.addEventListener('click', ()=>{ console.log('I was clicked');
+  });
+});
+
+closeModal.addEventListener('click', ()=>{ modal.close();}
+);
+
+
 
 // Smooth Scrolling
 $('#navbar a, .btn').on('click', function(event) {
