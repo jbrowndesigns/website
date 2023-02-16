669b2daa6f97d369273877a4bcb32049e84f241d
jbrowndesigns <brown.jdesign@gmail.com>
2023-02-15 23:04:33 -0800

    +Thank you page *js strict mode enabled*

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+'use strict';
+
+
 const openModal = document.querySelector('.open-btn');
 const closeModal = document.querySelector('.close-btn');
 const modal = document.querySelector('.modal');
@@ -18,15 +21,15 @@ closeModal.addEventListener('click', ()=>{ modal.close();}
 // Smooth Scrolling
 $('#navbar a, .btn').on('click', function(event) {
   if (this.hash !== '') {
-    event.preventDefault();
+      event.preventDefault();
 
-    const hash = this.hash;
+      const hash = this.hash;
 
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top - 80
-      },
-      800
-    );
-  }
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 80
+        },
+        800
+      );
+    }
 });
